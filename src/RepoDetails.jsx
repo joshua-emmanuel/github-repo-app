630ce220663e351c07b27cdd8c387e81b17c162b
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-04-19 07:42:46 +0100

    Style repo details component

--- src/RepoDetails.jsx
+++ src/RepoDetails.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./assets/css/RepoDetails.css";
+import githubMark from "./assets/images/github-mark.svg";
 
 function RepoDetails() {
   const [repoDetails, setRepoDetails] = useState({});
@@ -20,18 +21,33 @@ function RepoDetails() {
     <div className="repo-details">
       {Object.keys(repoDetails).length > 0 ? (
         <div className="wrapper">
-          <h1>{repoDetails.name}</h1>
-          <p>Description: {repoDetails.description ?? "No description"}</p>
-          <p>Main Language: {repoDetails.language}</p>
-          <p>Forks: {repoDetails.forks}</p>
-          <p>
-            Live-Site:{" "}
-            {repoDetails.homepage !== null ? (
-              <a href={repoDetails.homepage}>{repoDetails.homepage}</a>
-            ) : (
-              "None"
-            )}
-          </p>
+          <div className="repo-details__text">
+            <h1>
+              <img src={githubMark} alt="" />
+              <span>{repoDetails.name}</span>
+            </h1>
+            <div className="repo-details__body-text">
+              <p>Description: {repoDetails.description ?? "No description"}</p>
+              <p>Main Language: {repoDetails.language}</p>
+              <p>Forks: {repoDetails.forks}</p>
+              <p>
+                Live-Site:{" "}
+                {repoDetails.homepage !== null ? (
+                  <a href={repoDetails.homepage}>{repoDetails.homepage}</a>
+                ) : (
+                  "None"
+                )}
+              </p>
+            </div>
+          </div>
+          <div className="repo-details__btns">
+            <a className="repo-details__btn" href="/">
+              Go back home
+            </a>
+            <a className="repo-details__btn" href={repoDetails.html_url}>
+              View on github
+            </a>
+          </div>
         </div>
       ) : (
         <p>Loading...</p>
