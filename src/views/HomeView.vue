00110d9af690c0f24c842b8984786d0dd8870b76
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-05-31 12:10:00 +0100

    Create view for home page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import RepoList from '../components/RepoList.vue';
+</script>
+
+<template>
+  <header>
+    <div className="wrapper">
+      <h1>Joshua&apos;s Github Repos</h1>
+    </div>
+  </header>
+  <RepoList />
+</template>
+
+<style scoped>
+header {
+  padding-block: 2rem;
+  background-color: white;
+}
+
+header h1 {
+  text-align: center;
+  line-height: 1.1;
+}
+</style>
