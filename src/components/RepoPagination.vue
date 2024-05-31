e63a3fefc09105e1af2d88d74c691e79a6887d6b
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-05-31 13:35:20 +0100

    Extract repo pagination into its own component

---
+++ src/components/RepoPagination.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  isPrevButtonDisabled: Boolean,
+  isNextButtonDisabled: Boolean,
+  onPaginationButtonClick: Function,
+  numberOfPages: Number,
+  currentPage: Number
+});
+</script>
+
+<template>
+  <button :disabled="isPrevButtonDisabled" @click="onPaginationButtonClick(currentPage - 1)">
+    Prev
+  </button>
+  <button
+    v-for="number in numberOfPages"
+    @click="onPaginationButtonClick(number)"
+    :key="number"
+    :class="currentPage === number ? 'active' : ''"
+  >
+    {{ number }}
+  </button>
+  <button :disabled="isNextButtonDisabled" @click="onPaginationButtonClick(currentPage + 1)">
+    Next
+  </button>
+</template>
