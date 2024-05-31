1de298da9bb9d01d22bf71f27fb909385296d87b
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-05-31 12:12:16 +0100

    Use router before mounting app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue';
-import App from './App.vue';
 import './assets/css/index.css';
 
-createApp(App).mount('#app');
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/index';
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
