458385cee77c588390195382036e01c92a601bc5
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-05-31 13:03:06 +0100

    Add route to link to error boundary test

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createWebHistory, createRouter } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import RepoDetailsView from '../views/RepoDetailsView.vue';
 import PageNotFoundView from '../views/PageNotFoundView.vue';
+import ErrorBoundaryTest from '../components/ErrorBoundaryTest.vue';
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/repos/:repoName', component: RepoDetailsView },
-  { path: '/:pathMatch(.*)*', component: PageNotFoundView }
+  { path: '/:pathMatch(.*)*', component: PageNotFoundView },
+  { path: '/error', component: ErrorBoundaryTest }
 ];
 
 const router = createRouter({
