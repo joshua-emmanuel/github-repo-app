f40f23e60bc264861a443665131fef98c6915228
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-05-31 12:11:38 +0100

    Remove home page content and add router view

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <script setup>
-import RepoList from './components/RepoList.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <div>
-    <header>
-      <div className="wrapper">
-        <h1>Joshua&apos;s Github Repos</h1>
-      </div>
-    </header>
-    <RepoList />
+    <RouterView />
   </div>
 </template>
-
-<style scoped>
-header {
-  padding-block: 2rem;
-  background-color: white;
-}
-
-header h1 {
-  text-align: center;
-  line-height: 1.1;
-}
-</style>
