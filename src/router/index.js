38b3b17a8badd7de7d67df530f5892f340d075ef
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-05-31 12:25:30 +0100

    Add 404 page to routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createWebHistory, createRouter } from 'vue-router';
 
 import HomeView from '../views/HomeView.vue';
 import RepoDetailsView from '../views/RepoDetailsView.vue';
+import PageNotFoundView from '../views/PageNotFoundView.vue';
 
 const routes = [
   { path: '/', component: HomeView },
-  { path: '/repos/:repoName', component: RepoDetailsView }
+  { path: '/repos/:repoName', component: RepoDetailsView },
+  { path: '/:pathMatch(.*)*', component: PageNotFoundView }
 ];
 
 const router = createRouter({
