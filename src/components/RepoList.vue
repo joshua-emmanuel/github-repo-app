ed92cf43180bb1d9ab5d6a21b0d3d7ddc66a77c2
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-06-01 10:32:25 +0100

    Use reposearch in repolist component

--- src/components/RepoList.vue
+++ src/components/RepoList.vue
@@ -2,6 +2,7 @@
 import { ref, computed, onMounted } from 'vue';
 import { RouterLink } from 'vue-router';
 import RepoPagination from './RepoPagination.vue';
+import RepoSearch from './RepoSearch.vue';
 import GithubIcon from './icons/GithubIcon.vue';
 
 const searchQuery = ref('');
@@ -51,27 +52,7 @@ onMounted(() => {
 <template>
   <section class="repo-list">
     <div class="wrapper">
-      <form class="repo-list__search">
-        <svg
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-            clipRule="evenodd"
-          />
-        </svg>
-
-        <input
-          @input="onSearchInputChange"
-          type="text"
-          name="search-input"
-          placeholder="Search for repos"
-        />
-      </form>
+      <RepoSearch :onSearchInputChange="onSearchInputChange" />
       <div class="repos" v-if="filteredRepos.length > 0 && status === 'done'">
         <div
           v-for="repo in filteredRepos.slice(
@@ -120,40 +101,6 @@ onMounted(() => {
 
 .repo-list {
   margin-block: 3rem;
-}
-
-.repo-list__search {
-  margin-bottom: 2rem;
-  width: 100%;
-  position: relative;
-}
-
-.repo-list__search svg {
-  position: absolute;
-  left: 1rem;
-  top: 50%;
-  transform: translateY(-50%);
-
-  width: 20px;
-  height: 20px;
-  fill: #777e8a;
-}
-
-.repo-list__search input {
-  width: 100%;
-  padding: 1rem;
-  padding-left: 2.5rem;
-  border: 1px solid rgba(0, 0, 0, 0.15);
-  border-radius: 0.5rem;
-  background-color: white;
-}
-
-.repo-list__search input:hover {
-  border: 1px solid rgba(0, 0, 0, 0.35);
-}
-
-.repo-list__search input:focus {
-  outline-color: #082f49;
 }
 
 .repos {
